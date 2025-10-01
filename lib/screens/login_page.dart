3571227285b2fa6e13567cee8dec7e0cf853dfd0
박민준 <psss36786@gmail.com>
2025-10-01 15:07:41 +0900

    fix:before merge

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
@@ -9,6 +10,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
+  String email = "", password = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,83 +36,83 @@ class _LoginPage extends State<LoginPage> {
                   ),
                 ),
                 child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  // 키보드가 올라와서 만약 스크린 영역을 차지하는 경우 스크롤이 되도록
-                  // SingleChildScrollView으로 감싸 줌
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(labelText: 'Enter email'),
-                          keyboardType: TextInputType.emailAddress,
+                  padding: EdgeInsets.symmetric(horizontal: 80.0),
+
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (value) => email = value,
+                        decoration: InputDecoration(labelText: 'Enter email'),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        onChanged: (value) => password = value,
+                        decoration: InputDecoration(
+                          labelText: 'Enter password',
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Enter password',
+                        keyboardType: TextInputType.text,
+                        obscureText: true, // 비밀번호 안보이도록 하는 것
+                      ),
+                      SizedBox(height: 40.0),
+
+                      ElevatedButton(
+                        onPressed: () {
+                          print('login button');
+                          singInAction(email, password);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue, // 배경색
+                          foregroundColor: Colors.white, // 글자색
+                          minimumSize: const Size(
+                            double.infinity,
+                            50,
+                          ), // 최소 크기 (너비, 높이)
+                          elevation: 5, // 그림자 깊이
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5), // 둥근 모서리
                           ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true, // 비밀번호 안보이도록 하는 것
-                        ),
-                        SizedBox(height: 40.0),
 
-                        ElevatedButton(
-                          onPressed: () {
-                            print('login button');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue, // 배경색
-                            foregroundColor: Colors.white, // 글자색
-                            minimumSize: const Size(
-                              double.infinity,
-                              50,
-                            ), // 최소 크기 (너비, 높이)
-                            elevation: 5, // 그림자 깊이
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5), // 둥근 모서리
-                            ),
-
-                            textStyle: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF44A8BF),
-                            ),
+                          textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF44A8BF),
                           ),
-                          child: const Text('Login'),
                         ),
+                        child: const Text('Login'),
+                      ),
 
-                        SizedBox(height: 10.0),
+                      SizedBox(height: 10.0),
 
-                        ElevatedButton(
-                          onPressed: () {
-                            print('Sing In button');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white, // 배경색
-                            foregroundColor: Colors.blue, // 글자색
-                            minimumSize: const Size(
-                              double.infinity,
-                              50,
-                            ), // 최소 크기 (너비, 높이)
-                            elevation: 5, // 그림자 깊이
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Sing In button');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white, // 배경색
+                          foregroundColor: Colors.blue, // 글자색
+                          minimumSize: const Size(
+                            double.infinity,
+                            50,
+                          ), // 최소 크기 (너비, 높이)
+                          elevation: 5, // 그림자 깊이
 
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5), // 둥근 모서리
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 12,
-                            ),
-
-                            textStyle: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF44A8BF),
-                            ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5), // 둥근 모서리
                           ),
-                          child: const Text('Sing Up'),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 12,
+                          ),
+
+                          textStyle: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF44A8BF),
+                          ),
                         ),
-                      ],
-                    ),
+                        child: const Text('Sing Up'),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -121,3 +123,9 @@ class _LoginPage extends State<LoginPage> {
     );
   }
 }
+
+void singInAction(String email, String password) {
+  Fluttertoast.showToast(msg: "E:$email pw:$password");
+}
+
+void SingOutAction(String email, String password) {}
