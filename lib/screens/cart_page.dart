e1b5635eb16ad7d90e335bc60303be89415579c8
jung ui yeon <justicedestiny@cau.ac.kr>
2025-10-16 11:30:02 +0900

    스페이스

---
+++ lib/screens/cart_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatBotPage extends StatelessWidget{ // 이름은 같게 수정 요망
+  const ChatBotPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Text('챗봇 페이지');
+  }
+}
